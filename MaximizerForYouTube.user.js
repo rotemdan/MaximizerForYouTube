e885356e717cbfd735207be54b497841fba632c2
Rotem Dan <rotemdan@gmail.com>
2023-11-10 22:54:59 +0200

    Fix issue with auto expansion causing problems with menus. Update to latest jquery. Reformat code.

--- MaximizerForYouTube.user.js
+++ MaximizerForYouTube.user.js
@@ -4,31 +4,32 @@
 // @license     MIT
 // @author      Rotem Dan <rotemdan@gmail.com>
 // @match       https://www.youtube.com/*
-// @version     0.2.8
+// @version     0.2.9
 // @run-at      document-start
 // @grant       none
 // @namespace   https://github.com/rotemdan
 // @homepageURL https://github.com/rotemdan/MaximizerForYouTube
 // @require     https://cdn.jsdelivr.net/npm/js-cookie@2/src/js.cookie.min.js
-// @require     https://code.jquery.com/jquery-3.3.1.slim.min.js
+// @require     https://code.jquery.com/jquery-3.7.1.min.js
 // ==/UserScript==
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Utility definitions
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-const debugModeEnabled = true;
+const debugModeEnabled = false
+
 function log(...args) {
 	if (debugModeEnabled) {
-		console.log("[MaximizerForYoutube]", ...args)
+		console.log('[MaximizerForYoutube]', ...args)
 	}
 }
 
 // Try to emulate setImmediate() like execution:
 function setImmediate(func) {
-	const channel = new MessageChannel();
-	channel.port1.onmessage = () => func();
-	channel.port2.postMessage("");
+	const channel = new MessageChannel()
+	channel.port1.onmessage = () => func()
+	channel.port2.postMessage('')
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -38,7 +39,7 @@ function setImmediate(func) {
 // Install or uninstall full-size player page stylesheet if needed
 function installOrUninstallPlayerModIfNeeded() {
 	if (inWatchPage() && theaterModeEnabled()) {
-		if ($("#MaximizerForYouTube_PlayerMod").length == 0) {
+		if ($('#MaximizerForYouTube_PlayerMod').length == 0) {
 			const styleSheet = `
 					<style id='MaximizerForYouTube_PlayerMod' type='text/css'>
 						ytd-page-manager { margin-top: 0px !important; }
@@ -78,21 +79,22 @@ function installOrUninstallPlayerModIfNeeded() {
 						body::-webkit-scrollbar-thumb:hover {
 							background: #fff;
 						}
-					</style>`;
+					</style>`
 
-			$("head").append(styleSheet);
+			$('head').append(styleSheet)
 
-			log("Player mod installed");
+			log('Player mod installed')
 		}
 
-		if (pageScrolledToTop())
-			hideTopBar();
+		if (pageScrolledToTop()) {
+			hideTopBar()
+		}
 	} else {
-		if ($("#MaximizerForYouTube_PlayerMod").length > 0) {
-			$("#MaximizerForYouTube_PlayerMod").remove();
-			showTopBar();
+		if ($('#MaximizerForYouTube_PlayerMod').length > 0) {
+			$('#MaximizerForYouTube_PlayerMod').remove()
+			showTopBar()
 
-			log("Player mod uninstalled");
+			log('Player mod uninstalled')
 		}
 	}
 }
@@ -100,169 +102,191 @@ function installOrUninstallPlayerModIfNeeded() {
 // Automatically shows/hides the top bar based on different properties of the view.
 function installTopBarAutohide() {
 	function onPageScroll() {
-		if (!inWatchPage())
-			return;
+		if (!inWatchPage()) {
+			return
+		}
 
-		if (pageScrolledToTop() && theaterModeEnabled())
-			hideTopBar();
-		else
-			showTopBar();
+		if (pageScrolledToTop() && theaterModeEnabled()) {
+			hideTopBar()
+		} else {
+			showTopBar()
+		}
 	}
 
 	function onKeyDown(e) {
-		if (!inWatchPage() || !theaterModeEnabled())
-			return;
+		if (!inWatchPage() || !theaterModeEnabled()) {
+			return
+		}
 
 		if (e.which === 27) { // Handle escape key
-			log("esc");
+			log('esc')
 
 			if (pageScrolledToTop()) {
 				if (topBarIsVisible()) {
-					hideTopBar();
-					e.stopPropagation();
+					hideTopBar()
+					e.stopPropagation()
 				} else {
-					showTopBar();
+					showTopBar()
 
-					setTimeout(() => $("input#search").focus(), 50);
+					setTimeout(() => $('input#search').focus(), 50)
 				}
 			}
 		}
 	}
 
 	function installEscHandlerToSearchInput() {
-		let inputElement = $("input#search");
+		let inputElement = $('input#search')
 
 		if (inputElement.length > 0) {
-			inputElement.on("keydown", onKeyDown);
-			//inputElement[0].addEventListener("keydown", onKeyDown, true);
-			log("Esc handler installed on search input");
+			inputElement.on('keydown', onKeyDown)
+			//inputElement[0].addEventListener('keydown', onKeyDown, true)
+			log('Esc handler installed on search input')
 		} else {
-			setTimeout(() => installEscHandlerToSearchInput(), 50);
+			setTimeout(() => installEscHandlerToSearchInput(), 50)
 		}
 	}
 
-	installEscHandlerToSearchInput();
-	$(document).on("keydown", onKeyDown);
-	$(document).on("scroll", onPageScroll);
+	installEscHandlerToSearchInput()
+	$(document).on('keydown', onKeyDown)
+	$(document).on('scroll', onPageScroll)
 }
 
 // Continuously auto-focus the player keyboard input when some conditions are met.
 function installPlayerInputAutoFocus() {
 	function autoFocusIfNeeded() {
 		if (inWatchPage() && !topBarIsVisible()) {
-			getVideoContainer().focus();
+			getVideoContainer().focus()
 		}
 
-		setTimeout(autoFocusIfNeeded, 20);
+		setTimeout(autoFocusIfNeeded, 20)
 	}
 
-	autoFocusIfNeeded();
+	autoFocusIfNeeded()
 }
 
 function installPlayerKeyboardShortcutExtensions() {
 	// Install keyboard shortcut extensions
 	function onKeyDown(e) {
-		if (!inWatchPage())
-			return;
+		if (!inWatchPage()) {
+			return
+		}
 
-		if (getVideoContainer().is(":focus")) {
+		if (getVideoContainer().is(':focus')) {
 			if (e.ctrlKey) {
 				if (e.which === 37) { // Handle ctl + left key
-					var previousButton = $("a.ytp-prev-button")[0];
-					if (previousButton)
-						previousButton.click();
-				}
-				else if (e.which === 39) { // Handle ctl + right key
-					var nextButton = $("a.ytp-next-button")[0]
-					if (nextButton)
-						nextButton.click();
+					const previousButton = $('a.ytp-prev-button')[0]
+
+					if (previousButton) {
+						previousButton.click()
+					}
+				} else if (e.which === 39) { // Handle ctl + right key
+					const nextButton = $('a.ytp-next-button')[0]
+
+					if (nextButton) {
+						nextButton.click()
+					}
 				}
 			}
 		}
 	}
 
-	$(document).on("keydown", onKeyDown);
+	$(document).on('keydown', onKeyDown)
 }
 
 // Expands video description
 function ensureExpandedVideoDescription() {
 	setInterval(() => {
-		$("tp-yt-paper-button#expand").click()
-	}, 50);
+		if (!inWatchPage()) {
+			return
+		}
+
+		if ($('#ytd-watch-info-text').attr('detailed') == null) {
+			$('#description-inline-expander tp-yt-paper-button#expand').click()
+		}
+	}, 50)
 }
 
 // Expands video description
 function ensureModdedTheaterModeButton() {
 	setInterval(() => {
-		const playerModeButton = $("button.ytp-size-button");
+		if (!inWatchPage()) {
+			return
+		}
 
-		if (playerModeButton.length === 0 || playerModeButton.hasClass("MaximizerForYouTube_PlayerMod_Modded"))
-			return;
+		const playerModeButton = $('button.ytp-size-button')
 
-		playerModeButton.on("click", () => {
+		if (playerModeButton.length === 0 || playerModeButton.hasClass('MaximizerForYouTube_PlayerMod_Modded')) {
+			return
+		}
+
+		playerModeButton.on('click', () => {
 			setTimeout(() => {
-				installOrUninstallPlayerModIfNeeded();
+				installOrUninstallPlayerModIfNeeded()
 
-				const resizeEvent = new Event('resize');
-				window.dispatchEvent(resizeEvent);
+				const resizeEvent = new Event('resize')
+				window.dispatchEvent(resizeEvent)
 			}, 0)
 		})
 
-		playerModeButton.addClass("MaximizerForYouTube_PlayerMod_Modded");
-	}, 50);
+		playerModeButton.addClass('MaximizerForYouTube_PlayerMod_Modded')
+	}, 50)
 }
 
 function ensurePlayerIsAlwaysPaused() {
 	setInterval(() => {
-		const player = getVideoPlayer().get(0);
+		const player = getVideoPlayer().get(0)
+
 		if (player) {
-			player.pause();
+			player.pause()
 		}
-	}, 1000);
+	}, 1000)
 }
 
 function hideSPFLoadingBar() {
-	$("head").append("<style>#progress, yt-page-navigation-progress {display: none !important}</style>");
+	$('head').append('<style>#progress, yt-page-navigation-progress {display: none !important}</style>')
 }
 
 // Pauses playing videos in other tabs when a video play event is detected (works in both watch and channel page videos)
 function ensurePlayerAutoPause() {
-	const videoPlayer = getVideoPlayer();
+	const videoPlayer = getVideoPlayer()
 
 	if (videoPlayer.length > 0 &&
-		!videoPlayer.hasClass("MaximizerForYouTube_Modded_Autopause") &&
+		!videoPlayer.hasClass('MaximizerForYouTube_Modded_Autopause') &&
 		!inHomePage() &&
 		!inFeedPage() &&
 		!inSearchPage()) {
 		// Generate a random script instance ID
-		const instanceID = Math.random().toString();
+		const instanceID = Math.random().toString()
 
 		function onVideoPlay() {
-			log("onVideoPlay")
-			localStorage["MaximizerForYouTube_PlayingInstanceID"] = instanceID;
+			log('onVideoPlay')
+
+			localStorage['MaximizerForYouTube_PlayingInstanceID'] = instanceID
 
 			function pauseWhenAnotherPlayerStartsPlaying() {
-				if (localStorage["MaximizerForYouTube_PlayingInstanceID"] !== instanceID)
-					videoPlayer[0].pause();
-				else
-					setTimeout(pauseWhenAnotherPlayerStartsPlaying, 20);
+				if (localStorage['MaximizerForYouTube_PlayingInstanceID'] !== instanceID) {
+					videoPlayer[0].pause()
+				} else {
+					setTimeout(pauseWhenAnotherPlayerStartsPlaying, 20)
+				}
 			}
 
-			pauseWhenAnotherPlayerStartsPlaying();
+			pauseWhenAnotherPlayerStartsPlaying()
 		}
 
 		// If video isn't paused on startup, fire the handler immediately
-		if (!videoPlayer[0].paused)
-			onVideoPlay();
+		if (!videoPlayer[0].paused) {
+			onVideoPlay()
+		}
 
-		// Add event handler for the "play" event.
-		videoPlayer.on("play", onVideoPlay);
+		// Add event handler for the 'play' event.
+		videoPlayer.on('play', onVideoPlay)
 
 		// Mark the player as modded to ensure the autopause mod isn't installed again
-		videoPlayer.addClass("MaximizerForYouTube_Modded_Autopause");
+		videoPlayer.addClass('MaximizerForYouTube_Modded_Autopause')
 	}
 
-	setTimeout(ensurePlayerAutoPause, 50);
+	setTimeout(ensurePlayerAutoPause, 50)
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -271,46 +295,49 @@ function ensurePlayerAutoPause() {
 
 // Get the video player element
 function getVideoContainer() {
-	// Note: the channel page has another hidden video except the main one (if it exists). The hidden video doesn't have an "src" attribute.
-	return $("div.html5-video-player");
+	// Note: the channel page has another hidden video except the main one (if it exists). The hidden video doesn't have an 'src' attribute.
+	return $('div.html5-video-player')
 }
 
 // Get the video player element
 function getVideoPlayer() {
-	// Note: the channel page has another hidden video except the main one (if it exists). The hidden video doesn't have an "src" attribute.
-	return $('.html5-main-video').filter(function (index) { return $(this).attr("src") !== undefined });
+	// Note: the channel page has another hidden video except the main one (if it exists). The hidden video doesn't have an 'src' attribute.
+	return $('.html5-main-video').filter(function (index) {
+		return $(this).attr('src') !== undefined
+	})
 }
 
 // Get the top bar element
 function getTopBar() {
-	return $("#masthead-container");
+	return $('#masthead-container')
 }
 
 function showTopBar() {
-	getTopBar().css("visibility", "visible");
-	getTopBar().css("opacity", "1");
+	getTopBar().css('visibility', 'visible')
+	getTopBar().css('opacity', '1')
 }
 
 function hideTopBar() {
-	getTopBar().css("opacity", "0");
-	getTopBar().css("visibility", "hidden");
+	getTopBar().css('opacity', '0')
+	getTopBar().css('visibility', 'hidden')
 }
 
 function pageScrolledToTop() {
-	return $(document).scrollTop() === 0;
+	return $(document).scrollTop() === 0
 }
 
 function scrollPageToTopIfNeeded() {
 	setTimeout(() => {
 		if (inWatchPage() && $(document).scrollTop() > 0) {
-			log("Scrolling page to top");
-			$(document).scrollTop(0);
+			log('Scrolling page to top')
+
+			$(document).scrollTop(0)
 		}
-	}, 20);
+	}, 20)
 }
 
 function topBarIsVisible() {
-	return getTopBar().css("visibility") === "visible";
+	return getTopBar().css('visibility') === 'visible'
 }
 
 function inWatchPage() {
@@ -334,54 +361,55 @@ function locationPathname() {
 }
 
 function theaterModeEnabled() {
-	return Cookies.get("wide") === "1";
+	return Cookies.get('wide') === '1'
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Event handlers
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-let installInterval;
+let installInterval
 
 function onDocumentStart() {
-	log("onDocumentStart")
-	log("Script loaded, theater mode enabled:", theaterModeEnabled());
+	log('onDocumentStart')
 
-	//installOrUninstallPlayerModIfNeeded();
+	log('Script loaded, theater mode enabled:', theaterModeEnabled())
+
+	//installOrUninstallPlayerModIfNeeded()
 
 	installInterval = setInterval(() => {
-		installOrUninstallPlayerModIfNeeded();
-		//log("Trying to install mod")
-	}, 1);
+		installOrUninstallPlayerModIfNeeded()
+		//log('Trying to install mod')
+	}, 10)
 }
 
 function onDocumentEnd() {
-	log("onDocumentEnd");
+	log('onDocumentEnd')
 
 	clearInterval(installInterval)
-	installOrUninstallPlayerModIfNeeded();
+	installOrUninstallPlayerModIfNeeded()
 
-	//ensurePlayerIsAlwaysPaused();
+	//ensurePlayerIsAlwaysPaused()
 
-	hideSPFLoadingBar();
-	installTopBarAutohide();
-	installPlayerInputAutoFocus();
-	installPlayerKeyboardShortcutExtensions();
-	ensureExpandedVideoDescription();
+	hideSPFLoadingBar()
+	installTopBarAutohide()
+	installPlayerInputAutoFocus()
+	installPlayerKeyboardShortcutExtensions()
+	ensureExpandedVideoDescription()
 }
 
 function onWindowLoad() {
-	log("onWindowLoad");
+	log('onWindowLoad')
 
-	ensureModdedTheaterModeButton();
-	ensurePlayerAutoPause();
+	ensureModdedTheaterModeButton()
+	ensurePlayerAutoPause()
 }
 
 function onNavigation() {
-	log("onNavigation, new location:", location.href);
+	log('onNavigation, new location:', location.href)
 
-	//scrollPageToTopIfNeeded();
-	installOrUninstallPlayerModIfNeeded();
+	//scrollPageToTopIfNeeded()
+	installOrUninstallPlayerModIfNeeded()
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -389,14 +417,23 @@ function onNavigation() {
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 function startScript() {
-	document.addEventListener('DOMContentLoaded', onDocumentEnd, false);
-	$(window).on("load", onWindowLoad);
-	$(window).on("yt-navigate-start", () => { log("yt-navigate-start"), onNavigation() });
-	$(window).on("popstate", () => { log("popstate"); onNavigation() });
+	document.addEventListener('DOMContentLoaded', onDocumentEnd, false)
 
-	onDocumentStart();
+	$(window).on('load', onWindowLoad)
+
+	$(window).on('yt-navigate-start', () => {
+		log('yt-navigate-start')
+		onNavigation()
+	})
+
+	$(window).on('popstate', () => {
+		log('popstate')
+		onNavigation()
+	})
+
+	onDocumentStart()
 }
 
 if (window.self === window.top) {
-	startScript();
+	startScript()
 }
